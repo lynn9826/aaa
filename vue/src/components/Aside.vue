73cfa22bf407079ff6f51c206a04691c59d29057
lynn9826 <179783049@qq.com>
2023-02-04 16:41:19 +0800

    two

--- vue/src/components/Aside.vue
+++ vue/src/components/Aside.vue
@@ -5,7 +5,9 @@
 
         <el-menu
             style="width: 200px; min-height: calc(100vh - 50px)"
-            default-active="2"
+            default-active="user"
+            :default-openeds="['1']"
+            :unique-opened="true"
             class="el-menu-vertical-demo"
             @open="handleOpen"
             @close="handleClose"
@@ -13,32 +15,31 @@
           <el-sub-menu index="1">
             <template #title>
               <el-icon><location /></el-icon>
-              <span>Navigator One</span>
+              <span>系统管理</span>
             </template>
-            <el-menu-item-group title="Group One">
-              <el-menu-item index="1-1">item one</el-menu-item>
-              <el-menu-item index="1-2">item two</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group Two">
-              <el-menu-item index="1-3">item three</el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="1-4">
-              <template #title>item four</template>
-              <el-menu-item index="1-4-1">item one</el-menu-item>
-            </el-sub-menu>
+
+              <el-menu-item index="user">
+                <el-icon><Document /></el-icon>
+                <span>用户管理</span>
+              </el-menu-item>
+
           </el-sub-menu>
-          <el-menu-item index="2">
-            <el-icon><icon-menu /></el-icon>
-            <span>Navigator Two</span>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <el-icon><document /></el-icon>
-            <span>Navigator Three</span>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <el-icon><setting /></el-icon>
-            <span>Navigator Four</span>
-          </el-menu-item>
+
+          <el-sub-menu index="2">
+            <template #title>
+              <el-icon><location /></el-icon>
+              <span>系统管理2</span>
+            </template>
+
+            <el-menu-item index="user">
+              <el-icon><Document /></el-icon>
+              <span>用户管理2</span>
+            </el-menu-item>
+
+          </el-sub-menu>
+
+
+
         </el-menu>
       </el-col>
     </el-row>
--- vue/src/main.js
+++ vue/src/main.js
@@ -15,6 +15,6 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app =createApp(App).use(store).use(router).use(ElementPlus,{  size: 'small'}).mount('#app')
 
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
-}
+// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+//     app.component(key, component)
+// }
